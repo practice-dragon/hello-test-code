acd6c4c5000b1f8309ea947b15ddddb31e811d40
jihye1116 <jihyesim170@gmail.com>
2024-04-18 16:57:11 -0700

    체이닝과 subject

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,20 +1,25 @@
 beforeEach(() => {
   cy.visit('/login');
+  cy.findByLabelText('로그인').as('loginBtn');
 });
 
 it('이메일을 입력하지 않고 로그인 버튼을 클릭할 경우 "이메일을 입력하세요" 경고 메세지가 노출된다', () => {
-  cy.findByLabelText('로그인').click();
+  // get API -> Cypres에서 지저한 alias로 선언한 요소에 접근 가능
+  // 체이닝 형태로 테스트 코드 작성 -> 테스트 코드를 이해하기 쉬움. 코드 간결하게 작성 가능.
+  cy.get('@loginBtn').click();
 
   cy.findByText('이메일을 입력하세요').should('exist');
 });
 
 it('비밀번호를 입력하지 않고 로그인 버튼을 클릭할 경우 "비밀번호를 입력하세요" 경고 메세지가 노출된다', () => {
-  cy.findByLabelText('로그인').click();
+  cy.get('@loginBtn').click();
 
   cy.findByText('비밀번호를 입력하세요').should('exist');
 });
 
 it('잘못된 양식의 이메일을 입력한 뒤 로그인 버튼을 클릭할 경우 "이메일 양식이 올바르지 않습니다" 경고 메세지가 노출된다', () => {
+  // 1. 이메일 요소 조회 커맨드 실행 -> 완료되어야 type 커맨드로 subject가 넘어가 실행됨
+  // 2.type 커맨드로 텍스트 입력
   cy.findByLabelText('이메일').type('wrongemail#mail.com');
   cy.findByLabelText('로그인').click();
 
